5002f5f703c2be367ed15036694aa111ddc35381
Alex Ryan <alex.ryan@inspectorio.com>
2021-08-14 17:54:14 +0700

    Add DatasetLoader

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,6 +1,16 @@
+import pandas
+
 from pandas import DataFrame
 
 
 class DatasetLoader:
-    def load(self, path: str) -> DataFrame:
-        ...
+    SENTIMENT_COL = 0
+    CONTENT_COL = 1
+
+    @classmethod
+    def load(cls, path: str) -> DataFrame:
+        return pandas.read_csv(
+            path,
+            usecols=[cls.SENTIMENT_COL, cls.CONTENT_COL],
+            encoding='ISO-8859-1',
+        )
